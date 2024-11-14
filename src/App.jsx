ae6fa7075c5b06526cb99eb081d7610d4d14ff16
walidmg125@gmail.com <walidmg125@gmail.com>
2024-11-14 12:46:49 +0100

    fix: fixed Button componnet to support IconButton

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,37 @@
 import "./App.css";
 import { useState, useEffect } from "react";
-import { Container, Title, Form, Task, EditForm, Button, SearchForm } from "./components/index";
-import { FaSearch } from "react-icons/fa";
+import {
+  Container,
+  Title,
+  Form,
+  Task,
+  EditForm,
+  SearchForm,
+  Button,
+} from "./components/index";
 import { v4 as uuidv4 } from "uuid";
 uuidv4();
+import { CiSearch } from "react-icons/ci";
 
 function App() {
   const [todos, setTodos] = useState([]);
-  const [filteredTasks, setFilteredTasks] = useState([])
-  const [inputValue, setInputValue] = useState('')
-  
+  const [filteredTasks, setFilteredTasks] = useState([]);
+  const [inputValue, setInputValue] = useState("");
 
   const handleChange = (e) => {
-    setInputValue(e.target.value)
-  }
+    setInputValue(e.target.value);
+  };
 
   useEffect(() => {
-    setFilteredTasks(todos)
-  }, [todos])
+    setFilteredTasks(todos);
+  }, [todos]);
 
   useEffect(() => {
-    const filter = todos.filter(
-      task => task.description.toLowerCase().includes(inputValue)
-    )
-    setFilteredTasks(filter)
-  }, [inputValue])
+    const filter = todos.filter((task) =>
+      task.description.toLowerCase().includes(inputValue)
+    );
+    setFilteredTasks(filter);
+  }, [inputValue]);
 
   const addTask = (todo) => {
     setTodos([
@@ -70,8 +77,9 @@ function App() {
         <Title>Get Things Done!</Title>
 
         <Form addTask={addTask} />
-        
-        <SearchForm handleChange={handleChange}/>
+
+        <SearchForm handleChange={handleChange} />
+        <Button title="Submit" icon={<CiSearch />} varaint="primary" />
 
         {filteredTasks.map((task) =>
           task.isEditing ? (
--- src/components/Form/SearchForm.jsx
+++ src/components/Form/SearchForm.jsx
@@ -1,32 +1,35 @@
 import { Button } from "../index";
-import { useState} from "react";
-
+import { useState } from "react";
 
 const SearchForm = (props) => {
-    const [search, setSearch] = useState(false)
-    return (
-        <>
-            {
-                search ? (
-                    <div className="flex flex-row mb-7">
-                    <input
-                        type="text"
-                        placeholder="Search"
-                        onChange={props.handleChange}
-                        className="sm:w-96 border-2 border-violet-500 bg-violet-950 p-2 placeholder-slate-300 text-gray-100  focus:outline-none focus:border-violet-500"
-                    />
-                    <Button title= "Close" varaint="primary" onClick={() => setSearch(prevSearch => !prevSearch)}/>
-                    </div>
-                ) : (
-                    <div className="flex flex-row mb-7 justify-center">
-                    <Button title="Search" varaint="Search"  onClick={() => setSearch(prevSearch => !prevSearch)} />
-
-                    </div>
-                )
-            }
-
-        </>
-    );
+  const [search, setSearch] = useState(false);
+  return (
+    <>
+      {search ? (
+        <div className="flex flex-row mb-7">
+          <input
+            type="text"
+            placeholder="Search"
+            onChange={props.handleChange}
+            className="sm:w-96 border-2 border-violet-500 bg-violet-950 p-2 placeholder-slate-300 text-gray-100  focus:outline-none focus:border-violet-500"
+          />
+          <Button
+            title="Close"
+            varaint="primary"
+            onClick={() => setSearch((prevSearch) => !prevSearch)}
+          />
+        </div>
+      ) : (
+        <div className="flex flex-row mb-7 justify-center">
+          <Button
+            title="Search"
+            varaint="Search"
+            onClick={() => setSearch((prevSearch) => !prevSearch)}
+          />
+        </div>
+      )}
+    </>
+  );
 };
 
 export default SearchForm;
